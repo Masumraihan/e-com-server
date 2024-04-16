906b8020466fbcd03748b8e6fbd106a1936c770b
Masumraihan <mdmasumraihan1@gamil.com>
2024-04-16 18:41:07 +0600

    refactor:populate category from sub category

--- src/app/modules/subCategory/subCategory.service.ts
+++ src/app/modules/subCategory/subCategory.service.ts
@@ -13,7 +13,9 @@ const createSubCategoryIntoDb = async (payload: TSubCategory, user: JwtPayload) 
 
 const getAllSubCategoriesFromDb = async (query: Record<string, unknown>) => {
   const subCategorySearchableFields = ['subCategory'];
-  const result = new QueryBuilder(SubCategoryModel.find(), query).search(subCategorySearchableFields);
+  const result = new QueryBuilder(SubCategoryModel.find().populate('category'), query).search(
+    subCategorySearchableFields,
+  );
   const data = await result.modelQuery;
   return data;
 };

eb49740f054e0b99bce46ed19707d55b00a35442
Masumraihan <mdmasumraihan1@gamil.com>
2024-04-23 16:26:16 +0600

    doc:add postman doc link in readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# e-com-server
+```
+postman documentationhttps://documenter.getpostman.com/view/24929762/2sA3Bq5Bq9#7bec9418-70f2-430a-9b94-653146ae4869
+
+```
--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: ['http://localhost:3000', 'https://e-commerse-flax.vercel.app'],
     credentials: true,
+    
   }),
 );
 app.use(express.json());
